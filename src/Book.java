459eb719e9449597a903a69f675e0b8c4100a65b
nproshka <pris.stepa@mail.ru>
2022-09-01 19:10:55 +0300

    Сделал задание из следующего урока

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
 
     private final String nameBook;
@@ -22,5 +24,23 @@ public class Book {
 
     public void setYear(int year1) {
         this.year = year1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(nameBook, book.nameBook);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameBook);
+    }
+
+    @Override
+    public String toString() {
+        return "Today I read " + nameAuthor + "'" + nameBook + "'" + " written in " + year + ".";
     }
 }
